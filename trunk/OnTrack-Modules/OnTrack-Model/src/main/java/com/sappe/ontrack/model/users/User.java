21eba982914e7c66f4408f29c51a9e1997e722bf
marcelourreli81@gmail.com <marcelourreli81@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2012-09-15 20:08:46 +0000

    Models de Person, Role y User

---
+++ trunk/OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/users/User.java
@@ -0,0 +1,56 @@
+package com.sappe.ontrack.model.users;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+public class User extends Person {
+	
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="password")
+	private String password;
+	
+
+
+	@ManyToMany
+	@JoinTable(name="user_role", 
+		joinColumns={@JoinColumn(name="id_person")}, 
+		inverseJoinColumns={@JoinColumn(name="id_role")})		
+	/**
+	 * relación N a N entre user y roles 
+	 * un user es una person y posee igual id, se expresa con la tabla relacional user_role
+	 * que contiene id_person de la tabla person e id_role de la tabla role
+	 */
+	private List<Role> roles;
+		
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+}
