a8587f0c310043ba90de6164a364757b7aa3739b
marcelourreli81@gmail.com <marcelourreli81@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2012-09-15 20:40:43 +0000

    correcciones de hibernate

--- trunk/OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/users/Role.java
+++ trunk/OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/users/Role.java
@@ -1,14 +1,19 @@
 package com.sappe.ontrack.model.users;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="roles")
 public class Role {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_role")
 	private Integer id;
 	
 	@Column(name="role_name")
