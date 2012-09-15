29ec83c00a4c571f4186c9fe76696af5f49ea938
marcelourreli81@gmail.com <marcelourreli81@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2012-09-15 20:40:05 +0000

    correcciones de hibernate

--- trunk/OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/users/Person.java
+++ trunk/OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/users/Person.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 import javax.persistence.Table;
 
 
 @Entity
 @Table(name="persons")
+@Inheritance(strategy=InheritanceType.JOINED)
 public class Person {
 
 	@Id
