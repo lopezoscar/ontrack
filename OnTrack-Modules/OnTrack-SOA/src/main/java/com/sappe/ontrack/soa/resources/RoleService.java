426af84fe8daa236c4994276a53e954baeb30fa6
nicolas.d.reymundo@gmail.com <nicolas.d.reymundo@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2012-09-29 21:02:09 +0000

    Creado. Explota cuando trata de grabar en la DB.

--- OnTrack-Modules/OnTrack-SOA/src/main/java/com/sappe/ontrack/soa/resources/RoleService.java
+++ OnTrack-Modules/OnTrack-SOA/src/main/java/com/sappe/ontrack/soa/resources/RoleService.java
@@ -1,10 +1,13 @@
 package com.sappe.ontrack.soa.resources;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -28,4 +31,29 @@ public class RoleService {
 		Role role = roleManager.read(primaryKey);
 		return role; 
 	}
+	
+	@POST
+	@Path("createrole")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createRole(Role roleJson) {
+		roleManager.create(roleJson);
+		return Response.status(200).entity("Success").build();
+	} 
+	
+	@POST
+	@Path("updaterole")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void updateRole(Role roleJson) {
+		roleManager.update(roleJson);
+	}
+	
+	@POST
+	@Path("deleterole")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void deleteRole(Role roleJson) {
+		roleManager.delete(roleJson);
+	}
+
 }
