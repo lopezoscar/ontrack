39de6ac7e71f99fb5add1d9dd5da3b88c49cf4ce
marcelourreli81@gmail.com <marcelourreli81@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2012-09-29 21:02:40 +0000

    implementación método ws que trae lista de issue status

--- OnTrack-Modules/OnTrack-SOA/src/main/java/com/sappe/ontrack/soa/resources/IssueStatusService.java
+++ OnTrack-Modules/OnTrack-SOA/src/main/java/com/sappe/ontrack/soa/resources/IssueStatusService.java
@@ -1,6 +1,9 @@
 package com.sappe.ontrack.soa.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,5 +30,13 @@ public class IssueStatusService {
 	public IssueStatus getIssueStatusById(@PathParam("pk") Long primaryKey){
 		IssueStatus issueStatus = issueStatusManager.read(primaryKey);
 		return issueStatus;		
+	}
+	
+	@POST
+	@Path("getallissuestatus")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<IssueStatus> getAllIssueStatus(){
+		List<IssueStatus> issuesStatus = issueStatusManager.getAllIssueStatus();
+		return issuesStatus;
 	}
 }
