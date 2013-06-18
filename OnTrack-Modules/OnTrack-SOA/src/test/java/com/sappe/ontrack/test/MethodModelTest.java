82ad83022db654607e934a56b0bdf48ebc7ed735
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2013-06-18 03:05:45 +0000

    Se crea test que busca todas las url de los servicios y sus parametros.

---
+++ OnTrack-Modules/OnTrack-SOA/src/test/java/com/sappe/ontrack/test/MethodModelTest.java
@@ -0,0 +1,50 @@
+package com.sappe.ontrack.test;
+
+import java.util.List;
+
+public class MethodModelTest {
+	
+	private String name;
+	
+	private String path;
+	
+	private String method;
+	
+	private List<String> parameters;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public List<String> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(List<String> parameters) {
+		this.parameters = parameters;
+	}
+	
+	
+	
+	
+}
--- OnTrack-Modules/OnTrack-SOA/src/test/java/com/sappe/ontrack/test/ServicesTest.java
+++ OnTrack-Modules/OnTrack-SOA/src/test/java/com/sappe/ontrack/test/ServicesTest.java
@@ -1,7 +1,13 @@
 package com.sappe.ontrack.test;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 import org.junit.Test;
@@ -20,39 +26,108 @@ public class ServicesTest {
 	private ListableBeanFactory beanFactory;
 
 	@Autowired
-	private BeanFactory factory; 
+	private BeanFactory factory;
+	
+	public final static String baseURL = "http://localhost:8080/OnTrack-SOA/";
+	
+	
 	@Test
 	public void test() throws ClassNotFoundException{
 //		String[] beans = beanFactory.getBeanDefinitionNames();
 		Map<String,Object> beans = beanFactory.getBeansWithAnnotation(Path.class);
+		List<MethodModelTest> methods = new ArrayList<MethodModelTest>();
+		Map<String,List<MethodModelTest>> methodsByClass = new HashMap<String,List<MethodModelTest>>();
 		for (String bean : beans.keySet()) {
-			System.out.println(bean);
-			System.out.println(beans.get(bean));
+			methods = searchServicesIntoBean(beans.get(bean));
+			methodsByClass.put(bean, methods);
+		}
+		int total = 0;
+		for (String key : methodsByClass.keySet()) {
+			List<MethodModelTest> list =  methodsByClass.get(key);
+			int size = list.size();
+			total+=size;
+			System.out.println("Class: "+key+" - Cant Methods: "+size+"\n");
+			for (MethodModelTest m : list) {
+				System.out.println(m.getPath());
+			}
+			System.out.println("\n");
+		}
+		System.out.println(total);
+//		for (MethodModelTest m : methods) {
+////			System.out.println("Method: "+m.getName()+" - ");
+//			System.out.println(" - HTTP METHOD: "+m.getMethod());
+//			System.out.println(m.getPath()+"\n");
+////			System.out.println(m.getParameters().size());
+//		}
+	}
+	
+	private List<MethodModelTest> searchServicesIntoBean(Object bean) throws ClassNotFoundException{
+		List<MethodModelTest> methods = new ArrayList<MethodModelTest>();
+		Class clazz = Class.forName(bean.getClass().getCanonicalName());
+		
+		for (Method m : clazz.getDeclaredMethods()) {
+			int cantMethods = 0;
+			Path pathSrv = (Path)clazz.getAnnotation(Path.class);
+			String servicePath = pathSrv.value();
+			if (m.isAnnotationPresent(Path.class)) {
+				cantMethods++;
+				MethodModelTest method = new MethodModelTest();
+				boolean httpGET = false;
+				
+				Path methodPath = m.getAnnotation(Path.class);
+				String methodName = methodPath.value();
+				
+				if(m.isAnnotationPresent(POST.class)){
+					method.setMethod("POST");
+				}
+				if(m.isAnnotationPresent(GET.class)){
+					httpGET = true;
+					method.setMethod("GET");
+				}
+				
+				
+				method.setName(m.getName());
+				method.setParameters(extractParametersFromMethod(m));
+				method.setPath(buildPath(baseURL, servicePath, methodName, httpGET, method.getParameters()));
+				methods.add(method);
+			}
 		}
 		
-//		int methods = 0;
-//		for (String clazz : beans) {
-//			factory.getBean(arg0)
-//			Class testClass = Class.forName(clazz);
-//			
-//			for (Method m : testClass.getDeclaredMethods()) {
-//				if (m.isAnnotationPresent(Path.class)) {
-//					methods++;
-//					try {
-//						m.invoke(null);
-////						passed++;
-//					} catch (InvocationTargetException wrappedExc) {
-//						Throwable exc = wrappedExc.getCause();
-//						System.out.println(m + " failed: " + exc);
-//					} catch (Exception exc) {
-//						System.out.println("INVALID @Test: " + m);
-//					}
-//				}
-//			}
-//			
-//			System.out.println(methods);
-//		}
-		
-		
+		return methods;
+	}
+	
+	private String buildPath(String baseURL,String servicePath,String methodName,boolean httpGET,List<String> parameters){
+		StringBuffer sb = new StringBuffer();
+		sb.append(baseURL);
+		sb.append(servicePath);
+		sb.append("/");
+		sb.append(methodName);
+		sb.append("/");
+		if(!httpGET){
+			for (String param : parameters) {
+				sb.append("{");
+				sb.append(param);
+				sb.append("}");
+			}
+		}
+		String url = sb.toString();
+		url = clean(url);
+		return url;
+	}
+	
+	private String clean(String url){
+		url = url.replaceAll("//", "/");
+		url = url.replaceAll("http:/", "http://");
+		return url;
+	}
+	
+	private List<String> extractParametersFromMethod(Method m){
+		List<String> result = new ArrayList<String>();
+		Class[] parameters = m.getParameterTypes();
+		for (Class param : parameters) {
+			result.add(param.getCanonicalName());
+//			System.out.println(param);
+		}
+		return result;
 	}
 }
