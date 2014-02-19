f151d538c7c0d0c0a067f0d1ea8963c3cd41592d
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-02-19 03:52:17 +0000

    modal home para username

--- OnTrack-Modules/OnTrack-Web/src/main/webapp/js/profile.js
+++ OnTrack-Modules/OnTrack-Web/src/main/webapp/js/profile.js
@@ -2,7 +2,7 @@ function ProfileController($scope,$http,$location){
 
 	$scope.server = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack-SOA/";
 	$scope.webserver = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack/";
-    
+    $scope.userNameRequired = false;
     $http({method: 'GET', url: $scope.webserver+'currentUser',headers: {'Content-Type': 'application/json'}}).
 	  success(function(data, status, headers, config) {
 	   	$scope.currentUser = data;
@@ -12,10 +12,19 @@ function ProfileController($scope,$http,$location){
 	  });
 	  
 	 $scope.saveUser = function(user){
+	 	if(typeof $scope.currentUser.userName === "undefined" || $scope.currentUser.userName == null){
+	 		$scope.userNameRequired = true;
+	 		return;
+	 	};
+	 
+	 
 	 	$http({method: 'POST', url: $scope.server+'usersrv/updateuser',data:user, headers: {'Content-Type': 'application/json'}}).
 		  success(function(data, status, headers, config) {
 		   	$scope.currentUser = data;
 		   	$scope.saveOk = true;
+		   	$('#myModal').modal('hide');
+		   	//$('#myModal').modal(options);
+		   	
 		  }).
 		  error(function(data, status, headers, config) {
 		  	$scope.noUser = true;
