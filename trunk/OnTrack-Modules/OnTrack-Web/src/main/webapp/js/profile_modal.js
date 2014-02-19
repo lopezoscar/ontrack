bf96bb7f7d5b735f733475aa513a67d5c43a7a8c
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-02-19 03:52:17 +0000

    modal home para username

---
+++ trunk/OnTrack-Modules/OnTrack-Web/src/main/webapp/js/profile_modal.js
@@ -0,0 +1,27 @@
+function ProfileModal($scope,$http,$location){
+
+	$scope.server = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack-SOA/";
+	$scope.webserver = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack/";
+	
+
+    
+    $http({method: 'GET', url: $scope.webserver+'currentUser',headers: {'Content-Type': 'application/json'}}).
+	  success(function(data, status, headers, config) {
+	   	$scope.currentUser = data;
+	   	
+	   	if(typeof $scope.currentUser.userName === "undefined" || $scope.currentUser.userName == null){
+			var options = {
+						show:true,
+						keyboard: false,
+						backdrop: 'static'
+						};
+			
+			$('#myModal').modal(options);	
+	   	}
+	   	
+	  }).
+	  error(function(data, status, headers, config) {
+	  	$scope.noUser = true;
+	  });
+
+}
