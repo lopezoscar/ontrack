5f35fa51ab6e6e460a9125d47949b0b6fb910402
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-02-27 03:09:55 +0000

    Corrección Nombre de usuario ya existente

--- OnTrack-Modules/OnTrack-Web/src/main/webapp/js/profile.js
+++ OnTrack-Modules/OnTrack-Web/src/main/webapp/js/profile.js
@@ -3,6 +3,8 @@ function ProfileController($scope,$http,$location){
 	$scope.server = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack-SOA/";
 	$scope.webserver = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack/";
     $scope.userNameRequired = false;
+    $scope.userNameExist = false;
+    
     $http({method: 'GET', url: $scope.webserver+'currentUser',headers: {'Content-Type': 'application/json'}}).
 	  success(function(data, status, headers, config) {
 	   	$scope.currentUser = data;
@@ -11,25 +13,44 @@ function ProfileController($scope,$http,$location){
 	  	$scope.noUser = true;
 	  });
 	  
-	 $scope.saveUser = function(user){
-	 	if(typeof $scope.currentUser.userName === "undefined" || $scope.currentUser.userName == null){
-	 		$scope.userNameRequired = true;
-	 		return;
-	 	};
+	  $scope.saveUser = function(user){
+	  
+	  		var username = $scope.currentUser.userName;
+	  		 if(typeof username === "undefined" || username == null){
+			 		$scope.userNameRequired = true;
+			 		return;
+			 };
+			 
+    		$http({method: 'GET', url: $scope.server+'usersrv/existusername/'+username,headers: {'Content-Type': 'application/json'}}).
+			  success(function(data, status, headers, config) {
+			  
+			   	if(data == 'true'){
+			   		$scope.userNameExist = true;
+			   	}else{
+			   		$http({method: 'POST', url: $scope.server+'usersrv/updateuser',data:user, headers: {'Content-Type': 'application/json'}}).
+					  success(function(data, status, headers, config) {
+					   	$scope.currentUser = data;
+					   	$scope.saveOk = true;
+					   	$('#myModal').modal('hide');
+					   	
+					  }).
+					  error(function(data, status, headers, config) {
+					  	$scope.noUser = true;
+					  	$scope.saveFail = true;
+					  });
+					 }
+			  }).
+			  error(function(data, status, headers, config) {
+			  	$scope.noProjects = true;
+			  });
+    	};
+	  
 	 
+	 	
+	 	
+	 	
+	 	
 	 
-	 	$http({method: 'POST', url: $scope.server+'usersrv/updateuser',data:user, headers: {'Content-Type': 'application/json'}}).
-		  success(function(data, status, headers, config) {
-		   	$scope.currentUser = data;
-		   	$scope.saveOk = true;
-		   	$('#myModal').modal('hide');
-		   	//$('#myModal').modal(options);
-		   	
-		  }).
-		  error(function(data, status, headers, config) {
-		  	$scope.noUser = true;
-		  	$scope.saveFail = true;
-		  });
-		 };
+	 	
 
 }
