43f94996ef74a792ec182c31e57ede06dff3b4a1
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-02-27 01:19:16 +0000

    Se corrige lenguaje en datatables

--- trunk/OnTrack-Modules/OnTrack-Web/src/main/webapp/js/search-issue-filter.js
+++ trunk/OnTrack-Modules/OnTrack-Web/src/main/webapp/js/search-issue-filter.js
@@ -126,12 +126,33 @@ function retrieveIssuesByPOST($scope,$http,url,data){
 
 function parserResultToDataTableForIssues(data){
 	var source = {
+		oLanguage: {
+		    "sProcessing":     "Procesando...",
+		    "sLengthMenu":     "Mostrar _MENU_ registros",
+		    "sZeroRecords":    "No se encontraron resultados",
+		    "sEmptyTable":     "Ningún dato disponible en esta tabla",
+		    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+		    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+		    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+		    "sInfoPostFix":    "",
+		    "sSearch":         "Buscar:",
+		    "sUrl":            "",
+		    "sInfoThousands":  ",",
+		    "sLoadingRecords": "Cargando...",
+		    "oPaginate": {
+		        "sFirst":    "Primero",
+		        "sLast":     "Último",
+		        "sNext":     "Siguiente",
+		        "sPrevious": "Anterior"
+		    },
+		    "oAria": {
+		        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+		        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+		    }
+		},
 		sDom: 'T<"clear">lfrtip',
 		bJQueryUI : true,
 		sPaginationType: "full_numbers",
-		oLanguage: {
-            "sUrl": "datatables.spanish.txt"
-        },
 		oTableTools: {
 			"sSwfPath": "datatables/extras/TableTools/media/swf/copy_csv_xls_pdf.swf",
 			"aButtons": [
