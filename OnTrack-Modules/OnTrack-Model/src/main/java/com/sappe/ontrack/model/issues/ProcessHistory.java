051b8eda0cfc77e587d375cf9c5158b3c9357f3a
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-03-09 20:09:57 +0000

    Cambio de tipo Date de la var date en Processhistory

--- OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/issues/ProcessHistory.java
+++ OnTrack-Modules/OnTrack-Model/src/main/java/com/sappe/ontrack/model/issues/ProcessHistory.java
@@ -1,5 +1,7 @@
 package com.sappe.ontrack.model.issues;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -23,7 +25,7 @@ public class ProcessHistory {
 	private String status;
 	
 	@Column(name="date")
-	private String date;
+	private Date date;
 	
 	@ManyToOne
 	@JoinColumn(name="id_issue")
@@ -45,11 +47,11 @@ public class ProcessHistory {
 		this.status = status;
 	}
 
-	public String getDate() {
+	public Date getDate() {
 		return date;
 	}
 
-	public void setDate(String date) {
+	public void setDate(Date date) {
 		this.date = date;
 	}
 	
