01e4a6d45d9f2d672fe047b2035b8e87564f62de
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-03-09 20:11:45 +0000

    Se agregan estadísticas por proyecto

--- OnTrack-Modules/OnTrack-Web/src/main/webapp/js/stats.js
+++ OnTrack-Modules/OnTrack-Web/src/main/webapp/js/stats.js
@@ -2,41 +2,123 @@ function StatsController($scope,$http,$location){
 
 	$scope.server = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack-SOA/";
 	$scope.webserver = $location.$$protocol+"://"+$location.$$host+":"+$location.$$port+"/OnTrack/";
-
+	$scope.projects = [];
+	$scope.issuesBySelectedProject = [];
+	$scope.showDateRange = false;
+	$scope.fromDateEmpty = false;
+	$scope.toDateEmpty = false;
+	$scope.fromDateAfterToDate = false;
+	
+	$scope.searchIssuesFromProject = function(){
+	
+			$scope.lookupService = "issuesrv/listIssuesByUserFromProject"; 
+	  
+			$http({method: 'POST', url: $scope.server+$scope.lookupService,data:$scope.currentUser,headers: {'Content-Type': 'application/json'}}).
+			  success(function(data, status, headers, config) {
+			  		$scope.issuesForProject = data;
+			  		$scope.fillProjectList();
+			  		var rows = parseChartDataForIssueByProject($scope.issuesForProject);
+			  		drawChart(rows,'cant_issues_div','Issues Por Proyecto');
+			  		
+			  		
+			  }).error(function(data, status, headers, config) {
+			  	$scope.noUser = true;
+			  });
+	};
+	
 	$http({method: 'GET', url: $scope.webserver+'currentUser',headers: {'Content-Type': 'application/json'}}).
 	  success(function(data, status, headers, config) {
 	   	$scope.currentUser = data;
+	   	
 	   	
     	$http({method: 'POST', url: $scope.server+"issuesrv/listIssuesByUser",data:$scope.currentUser,headers: {'Content-Type': 'application/json'}}).
 		  success(function(data, status, headers, config) {
 		   	$scope.issues = data;
 		   	var rows = parseChartDataCurrentStatus($scope.issues);
-		   	drawChart(rows,'status_div','Mis Issues Por Estado Status');
+		   	drawChart(rows,'issues_status_div','Mis Issues Por Estado Status');
 		   	rows = parseChartDataIssueType($scope.issues);
-		   	drawChart(rows,'type_div','Mis Issues Por Tipo de Issue');
+		   	drawChart(rows,'issues_type_div','Mis Issues Por Tipo de Issue');
 		})
 		.error(function(data, status, headers, config) {
 		  	$scope.noIssues = true;
-		  });;
-		  
+		  });
+		 
+		  $scope.searchIssuesFromProject();
 		
-		$scope.lookupService = "issuesrv/listIssuesByUserFromProject"; 
-		  
-		$http({method: 'POST', url: $scope.server+$scope.lookupService,data:$scope.currentUser,headers: {'Content-Type': 'application/json'}}).
-		  success(function(data, status, headers, config) {
-		  		$scope.issuesForProject = data;
-		  		var rows = parseChartDataForIssueByProject($scope.issuesForProject);
-		  		drawChart(rows,'issues_div','Issues Por Proyecto');
-		  		
-		  		
-		  }).error(function(data, status, headers, config) {
-		  	$scope.noUser = true;
-		  });
-		  
-		  
-		}).error(function(data, status, headers, config) {
-		  	$scope.noUser = true;
-		  });
+	
+		
+	}).error(function(data, status, headers, config) {
+			  	$scope.noUser = true;
+	});
+		
+	$scope.updateCharts = function(){
+			$scope.issuesBySelectedProject = [];
+	
+			$scope.filterIssuesByProject();
+			
+			var rows = parseChartDataCurrentStatus($scope.issuesBySelectedProject);
+		   	drawChart(rows,'status_div','Issues Por Estado Status');
+		   	rows = parseChartDataIssueType($scope.issuesBySelectedProject);
+		   	drawChart(rows,'type_div','Issues Por Tipo de Issue');
+		   	var rows = parseChartDataForIssueByProject($scope.issuesBySelectedProject);
+			drawChart(rows,'issues_div','Issues');
+		
+	};
+
+		$scope.fillProjectList = function(){
+		
+			var projectsIds = [];
+			angular.forEach($scope.issuesForProject,function(issue,itemNo){
+				if(projectsIds.indexOf(issue.project.id) < 0){
+					projectsIds.push(issue.project.id);
+					$scope.projects.push(issue.project);
+				}
+			});
+		};
+		 
+		 $scope.filterIssuesByProject = function(){
+		 	angular.forEach($scope.issuesForProject,function(issue,itemNo){
+		 		if(issue.project.id == $scope.selectedProject.id){
+		 			$scope.issuesBySelectedProject.push(issue);
+		 		}
+		 	});
+		 };
+		 
+	 $scope.showDateRange = function(){
+	 	$scope.showDateRange = true;
+	 };
+	 	
+	$scope.createBBChart = function(){
+		
+		$scope.fromDateEmpty = false;
+		$scope.toDateEmpty = false;
+		$scope.fromDateAfterToDate = false;
+	
+		var from = document.getElementById("from").value;
+		var to = document.getElementById("to").value;
+		
+		if(typeof from === "undefined" || from == ""){
+			$scope.fromDateEmpty = true;
+			return;
+		}
+		if(typeof to === "undefined" || to == ""){
+			$scope.toDateEmpty = true;
+			return;
+		}
+		var fromDate = new Date(from);
+		var toDate = new Date(to);
+		
+		if(fromDate > toDate){
+			$scope.fromDateAfterToDate = true;
+			return;
+		}
+		
+		
+		
+	};
+		 	  
+				  
+				
 		  
 		  
 		    
