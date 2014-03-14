4f5a649d95483d6a5d5924c2c6ad3023b45f5cdd
lopezoscar.job@gmail.com <lopezoscar.job@gmail.com@d0741335-bae2-7d32-ae51-64deefa76e2a>
2014-03-14 00:22:15 +0000

    Onkeypress Enter en saveIssueType y saveIssueStatus. Corrección bug eliminar status

--- OnTrack-Modules/OnTrack-Web/src/main/webapp/js/create-project.js
+++ OnTrack-Modules/OnTrack-Web/src/main/webapp/js/create-project.js
@@ -4,7 +4,6 @@ function CreateProjectCtrl($scope,$http,$location){
 	$scope.selectedMember = "";
 	$scope.currentProjectID = GetURLParameter("project");
 	$scope.modifyStatus = false;
-	//$scope.members = [{"selected":false,"name":"Daniel Galanti","email":"dgalanti@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/64ac740a52fc9e"},{"selected":false,"name":"arielaguirre1420@gmail.com","email":"arielaguirre1420@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/c6f1850f2fb5ed"},{"selected":false,"name":"Gonzalez Mariela","email":"mgonzalez@escueladavinci.net","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/3e516260eef2ebe"},{"selected":false,"name":"Pablo Sebastián Reitano","email":"pablo.reitano@softtek.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/449a107897e0f33"},{"selected":false,"name":"Agostina Sysone","email":"","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/4981aea09f156e9"},{"selected":false,"name":"Pablo Pallocchi","email":"pablopallocchi@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/56346228fbc91fc"},{"selected":false,"name":"Roselis Guzmán","email":"rguzman@sysone.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/57db2118a77a322"},{"selected":false,"name":"toluispo@hotmail.com","email":"toluispo@hotmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/5b8ca91880639fa"},{"selected":false,"name":"Santiago Lohigorry","email":"santiago.lohigorry@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/6af932f0c1e425a"},{"selected":false,"name":"Robert Anderson","email":"","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/6d927a40c5b1b3e"},{"selected":false,"name":"Cristian Mielgo","email":"cristian.mielgo@cesvi.com.ar","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/6dc2d040f6c9263"},{"selected":false,"name":"Pablo Romanos","email":"pabloromanos@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/7e6ca7d08ab62c8"},{"selected":false,"name":"Gustavo Martinez","email":"gmartinez@datatech.com.ar","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/7f75160094efbd4"},{"selected":false,"name":"Nicolas Gladkoff","email":"ngladkoff@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/80b6f8f899a5e20"},{"selected":false,"name":"Alejandro Gaete","email":"agaete.rsa@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/878111d8acdaa59"},{"selected":false,"name":"maxilimeres@gmail.com","email":"maxilimeres@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/8acb8140bf874de"},{"selected":false,"name":"Romero, Sergio","email":"sromero@rsaelcomercio.com.ar","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/90c12678c1d7fd4"},{"selected":false,"name":"Matias Antunez","email":"matias.antunez@cardinalsisa.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/988047f8c179e4b"},{"selected":false,"name":"Laura Vazquez Profe","email":"","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/9a25d700b9cfd52"},{"selected":false,"name":"Cecilia Bietti","email":"cbietti@gmail.com","photoLink":"https://www.google.com/m8/feeds/photos/media/lopezoscar.job%40gmail.com/9cd25ce8abb307c"}]
 	$scope.members = [];
 	$scope.issueTypes = [];
 	$scope.issueProperties = [];
@@ -14,6 +13,16 @@ function CreateProjectCtrl($scope,$http,$location){
 	$scope.projectNameNotFound = false;
 	$scope.hideLoadContacts = false;
 	$scope.isAdminError = false;
+	
+	
+	    
+    $scope.saveIssueTypeForKeyPress = function(ev) {
+        $scope.pressed = ev.which;
+	    if (ev.which==13){
+	    	saveIssueType(desc);
+	    	
+	    }
+	};
 	
 	$scope.redirect = function(){
 		$scope.$apply(function() { $location.path("home.html"); });
@@ -365,7 +374,8 @@ function CreateProjectCtrl($scope,$http,$location){
 		var itIdx = $scope.issueTypes.indexOf(status.type);
 		var issueType = $scope.issueTypes[itIdx];
 		
-		issueType.status.splice(status,1);
+		var statusIdx = issueType.status.indexOf(status);
+		issueType.status.splice(statusIdx,1);
 	};
 	
 	$scope.updateCurrentStatus = function(type){
